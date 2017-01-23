1a0085b1ff43151052e4c05e688f9211f5ea47d6
Kostas Bariotis <konmpar@gmail.com>
2017-01-23 14:30:49 +0200

    disable autocomplete

--- app/App.js
+++ app/App.js
@@ -79,16 +79,31 @@ export default class App extends React.Component {
               <div className={this.state.activeStep === 1 ? styles.activeStep : styles.inactiveStep}>
                 <div className="form-group">
                   <label htmlFor="repo">Repository URL</label>
-                  <input className={`form-control ${styles.customControl}`} id="repo" type="text" onKeyUp={this.keyup.bind(this)} placeholder="e.g. https://github.com/kbariotis/throw.js"/>
+                  <input
+                    className={`form-control ${styles.customControl}`}
+                    id="repo"
+                    type="text"
+                    onKeyUp={this.keyup.bind(this)}
+                    placeholder="e.g. https://github.com/kbariotis/throw.js"
+                    autoComplete="off"
+                    />
                   <div className={`row ${styles.actions}`}>
                     <div className="col-sm-6">
                       {this.state.activeStep !== 1 &&
-                        <button className={`btn btn-default ${styles.customDefaultButton}`}  onClick={this.startOver.bind(this)}>Reset</button>
+                        <button
+                          className={`btn btn-default ${styles.customDefaultButton}`}
+                          onClick={this.startOver.bind(this)}>
+                            Reset
+                        </button>
                       }
                     </div>
                     <div className="col-sm-6 text-right">
                       {this.state.repo &&
-                        <button className={`btn btn-default ${styles.customPrimaryButton}`} onClick={this.getLabels.bind(this)}>Next</button>
+                        <button
+                          className={`btn btn-default ${styles.customPrimaryButton}`}
+                          onClick={this.getLabels.bind(this)}>
+                            Next
+                        </button>
                       }
                     </div>
                   </div>
@@ -98,7 +113,12 @@ export default class App extends React.Component {
                 <div className="form-group">
                   <label htmlFor="labels">Repo</label>
                   {this.state.labels.length > 0 &&
-                    <select className={`form-control ${styles.customControl}`} id="labels" onChange={this.chooseLabel.bind(this)}>
+                    <select
+                      className={`form-control ${styles.customControl}`}
+                      id="labels"
+                      onChange={this.chooseLabel.bind(this)}
+                      autoComplete="off"
+                    >
                       {this.state.labels.map((label, id) => {
                         return <option id={label.id} key={id}>{label.name}</option>;
                       })}
@@ -107,7 +127,11 @@ export default class App extends React.Component {
                   <div className={`row ${styles.actions}`}>
                     <div className="col-sm-6">
                       {this.state.activeStep !== 1 &&
-                        <button className={`btn btn-default ${styles.customDefaultButton}`}  onClick={this.startOver.bind(this)}>Reset</button>
+                        <button
+                          className={`btn btn-default ${styles.customDefaultButton}`}
+                          onClick={this.startOver.bind(this)}>
+                            Reset
+                        </button>
                       }
                     </div>
                   </div>
@@ -116,15 +140,30 @@ export default class App extends React.Component {
               <div className={this.state.activeStep === 3 ? styles.activeStep : styles.inactiveStep}>
                 <div className="form-group">
                   <label htmlFor="email">Email</label>
-                  <input className={`form-control ${styles.customControl}`} id="email" type="text" onKeyUp={this.validateEmail.bind(this)}/>
+                  <input
+                    className={`form-control ${styles.customControl}`}
+                    id="email"
+                    type="text"
+                    onKeyUp={this.validateEmail.bind(this)}
+                    autoComplete="off"
+                  />
 
                   <div className={`row ${styles.actions}`}>
                     <div className="col-sm-6">
-                      <button className={`btn btn-default ${styles.customDefaultButton}`}  onClick={this.startOver.bind(this)}>Reset</button>
+                      <button
+                        className={`btn btn-default ${styles.customDefaultButton}`}
+                        onClick={this.startOver.bind(this)}>
+                          Reset
+                      </button>
                     </div>
                     <div className="col-sm-6 text-right">
                       {this.state.email &&
-                        <button className={`btn btn-default ${styles.customPrimaryButton}`}  disabled={!this.state.email} onClick={this.setEmail.bind(this)}>Next</button>
+                        <button
+                          className={`btn btn-default ${styles.customPrimaryButton}`}
+                          disabled={!this.state.email}
+                          onClick={this.setEmail.bind(this)}>
+                            Next
+                        </button>
                       }
                     </div>
                   </div>
@@ -136,10 +175,19 @@ export default class App extends React.Component {
                 </p>
                 <div className={`row ${styles.actions}`}>
                     <div className="col-sm-6">
-                      <button className={`btn btn-default ${styles.customDefaultButton}`}  onClick={this.startOver.bind(this)}>Reset</button>
+                      <button
+                        className={`btn btn-default ${styles.customDefaultButton}`}
+                        onClick={this.startOver.bind(this)}>
+                          Reset
+                      </button>
                     </div>
                     <div className="col-sm-6 text-right">
-                      <button className={`btn btn-default ${styles.customPrimaryButton}`}  disabled={!this.state.labelChosen} onClick={this.subscribe.bind(this)}>Subscribe</button>
+                      <button
+                        className={`btn btn-default ${styles.customPrimaryButton}`}
+                        disabled={!this.state.labelChosen}
+                        onClick={this.subscribe.bind(this)}>
+                          Subscribe
+                      </button>
                     </div>
                   </div>
               </div>
